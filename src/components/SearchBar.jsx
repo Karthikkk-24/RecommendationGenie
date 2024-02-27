5609fc8926b974a9ff8a7010b327dcbaced31e64
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-02-27 22:42:45 +0530

    Added Search Bar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SearchBar() {
+  return (
+    <div className='search_container'>
+      <input type="text" placeholder="Search..." />
+    </div>
+  )
+}
